80e0f4e42cb834fa45c34e85198af7764d8ff7d8
andrew3467 <apgraser2@gmail.com>
2025-01-01 19:00:35 -0500

    Update

--- src/Monopoly/main.cpp
+++ src/Monopoly/main.cpp
@@ -2,15 +2,21 @@
 
 #include "Tungsten.h"
 
+std::unique_ptr<Tungsten::Camera> mainCamera;
 
 void Update()
 {
-    TUNGSTEN_INFO("Hello World!");
+    Tungsten::Renderer::StartScene(mainCamera->GetProjection());
+
+    Tungsten::Renderer::DrawCube(glm::vec3(0), glm::vec3(1), glm::vec3(1,0,1));
+    Tungsten::Renderer::DrawCube(glm::vec3(1,1.2,0), glm::vec3(1, 2, 1), glm::vec3(0,1,1));
 }
 
 int main()
 {
     Tungsten::Engine engine("Monopoly");
+
+    mainCamera = std::make_unique<Tungsten::Camera>();
 
     engine.Run(&Update);
 
